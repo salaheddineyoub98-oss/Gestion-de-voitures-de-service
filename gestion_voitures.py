8a505b2c5d9bf302395f015fc3053571c4dde73c
Salah Eddine Youb <salah.youb@email.com>
2026-03-07 16:09:22 -0500

    :creation de la calss Employe avec ses attributs

---
+++ gestion_voitures.py
@@ -0,0 +1,8 @@
+class Employe:
+    def __init__(self,numeropermis,nom,prenome):
+        self.numeropermis=numeropermis
+        self.nom=nom
+        self.prenom=prenome
+        self.voitureService=None
+    
+        
